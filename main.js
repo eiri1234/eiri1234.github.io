416669629d9b71b52657bff54c7ca4e0125c6f92
eiri1234 <eiri1234@gmail.com>
2021-03-10 08:16:21 -0600

    added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const countEl = document.getElementById("count");
+const countEl = document.getElementById("counter");
+
+updateVisitorCount();
 
 function updateVisitorCount() {
-    fetch('https://api.countapi.xyz/update/documentaryworld.net/eiri1234/?amount=1')
+    fetch("https://api.countapi.xyz/update/documentaryworld.net/eiri1234/?amount=1")
       
        .then(res => res.json())
        .then(res => {
