934aa3c92184c184f24931e32748d2b1379937c7
eiri1234 <eiri1234@gmail.com>
2021-07-01 15:14:48 -0500

    add

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import countapi from 'countapi-js';
+function myFunction(linkName){
+    link = document.getElementById("sky");
+    link.src = linkName;
+    btn1.style.color="black";
+}
 
-countapi.visits().then((result) => {
-    console.log(result.value);
-});
+function myFunction1(linkName){
+link = document.getElementById("drudge");
+link.src = linkName;
 
+}
+
+function myFunction2(linkName){
+ link = document.getElementById("infowars");
+ link.src = linkName;
+
+}
