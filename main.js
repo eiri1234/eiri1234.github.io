437ce5ddf903b49a735cbe444e68121ac3d62eaf
eiri1234 <eiri1234@gmail.com>
2021-11-05 18:07:08 -0500

    add

--- main.js
+++ main.js
@@ -1,63 +1,17 @@
-function myFunction(linkName){
-    link = document.getElementById("sky");
-    link.src = linkName;
-    
+
+function myFunction1(linkname){
+  link = document.getElementById("control");
+  link.src = linkname;
 }
 
-function myFunction1(linkName){
-link = document.getElementById("drudge");
-link.src = linkName;
 
+function myFunction2(linkname){
+  link = document.getElementById("control");
+  link.src = linkname;
 }
 
-function myFunction2(linkName){
- link = document.getElementById("infowars");
- link.src = linkName;
 
+function myFunction3(linkname){
+  link = document.getElementById("control");
+  link.src = linkname;
 }
-
-function myFunction4(linkName){
-    link = document.getElementById("sky");
-    link.src = linkName;
-}
-
-function myFunction5(skylink,aplink,infolink){
-    var sky = document.getElementById("sky");
-    var drudge = document.getElementById("drudge");
-    var infowars = document.getElementById("infowars");
-
-    sky = document.getElementById("sky");
-    sky.src = skylink;
-
-    ap = document.getElementById("drudge");
-    ap.src = aplink;
-
-    info = document.getElementById("infowars");
-    info.src = infolink;
-}
-
-function myFunction6(linkName){
-    link = document.getElementById("drudge");
-    link.src = linkName;
-}
-
-function myFunction7(linkName){
-    link = document.getElementById("infowars");
-    link.src = linkName;
-}
-
-function myFunction8(linkName){
-    link = document.getElementById("infowars");
-    link.src = linkName;
-}
-
-function myFunction9(linkName){
-    link = document.getElementById("sky");
-    link.src = linkName;
-}
-
-function myFunction10(linkName){
-    link = document.getElementById("drudge");
-    link.src = linkName;
-}
-
