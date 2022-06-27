8a8d6d148e9cd6b6813aa416ae852253c1024439
eiri1234 <eiri1234@gmail.com>
2022-06-27 16:58:01 -0500

    add

--- camsmain.js
+++ camsmain.js
@@ -19,3 +19,8 @@ function myFunction1(linkname){
     link = document.getElementById("control");
     link.src = linkname;
   }
+
+  function myFunction5(linkname){
+    link = document.getElementById("control");
+    link.src = linkname;
+  }
